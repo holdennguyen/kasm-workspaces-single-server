99ae8d2d1ebc678de4a479ac71f4b8fc7d2ded73
Hung. Minh Nguyen <89327966+holdennguyen@users.noreply.github.com>
2023-04-08 11:34:56 +0700

    update to kasm ver1.13

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /tmp
-curl -O https://kasm-static-content.s3.amazonaws.com/kasm_release_1.12.0.d4fd8a.tar.gz
-tar -xf kasm_release_1.12.0.d4fd8a.tar.gz
+curl -O https://kasm-static-content.s3.amazonaws.com/kasm_release_1.13.0.0e0f9b.tar.gz
+tar -xf kasm_release_1.13.0.0e0f9b.tar.gz
 yes | bash kasm_release/install.sh
